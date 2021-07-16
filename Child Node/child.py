630753d4252925f4f0c0ad8c47465078a505fe98
jake-ephraim <ephraimakolo2017@gmail.com>
2021-07-16 19:18:31 +0100

    created the send and sign function in child. also installed flask

--- Child Node/child.py
+++ Child Node/child.py
@@ -1,1 +1,37 @@
-print('hello')
+import requests
+
+def sign(data:dict):
+    signature = None
+    return signature
+
+def send(mother:str, data:dict):
+    '''
+    Sends sign's and sends data to Mother Node.
+
+        mother: str
+            address of the mother node
+        
+        data: dict
+            sender: str
+                public address of the sender
+            receiver: str
+                public address of the receiver
+            amount: str
+                amount to transfer from sender to receiver
+            token: str
+                amount to transfer from sender to first node (transaction fees)
+    '''
+    assert type(data) == dict
+    assert len(data) == 4
+    assert type(mother) == str
+    for object in data.values, :
+        assert type(object) == str
+    signedData = {
+        'sender': data.sender,
+        'receiver': data.receiver,
+        'amount': data.amount,
+        'token': data.token,
+        'signature': sign(data)
+    }
+
+    
