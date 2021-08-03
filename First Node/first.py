9b28d797490f45b8fa226cd4939ae32569492c51
jake-ephraim <ephraimakolo2017@gmail.com>
2021-08-03 12:34:47 +0100

    build first.py minedBlock() and created templates.

--- First Node/first.py
+++ First Node/first.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from datetime import datetime
+
+from rsa import key
 
 
 class FirstNode:
@@ -17,6 +20,7 @@ class FirstNode:
         #     print('test upload:', r)
         # ## End of Upload Test Blocks
         self.mother = mother
+        self.public_key = ''
     
     def blockCount(self) -> int:
         r = requests.get(self.mother + 'blockcount')
@@ -29,9 +33,54 @@ class FirstNode:
         assert r.status_code == 200
         return r.json()
     
+    def getChain(self, postion:str):
+        '''
+        get the blockchain (ledger)
+        
+        position: str
+            can range from -1 to infinity
+            -1: all the transaction blocks(complete history/ledger)
+            0-infinity: the specific block
+        '''
+        pass
+
+    
+    def getDifficulty(self) -> int:
+        '''
+        get difficulty from server
+        '''
+        pass
+    
+    def getCurrentHash(self) -> str:
+        '''
+        get last uploaded block's hash
+        '''
+        pass
+
+    def getHash(self, block) -> str:
+        '''
+        compute the hash of the current block 
+        '''
+        order = ('time', 'difficulty', 'verifier', 'previousHash')
+        nonce = 0
+        pass
+
+    def blocksVerified(self, block):
+        pass
+
+    def summitMinnedBlock(self, block):
+        pass
+    
     def mineBlock(self, index):
         block = self.getBlock(index)
-        pass
+        block = self.blocksVerified(block)
+        block['time'] = datetime.now().strftime("%I:%M%p %B %d %Y")
+        block['difficulty'] = self.getDifficulty()
+        block['verifier'] = self.public_key
+        block['previousHash'] = self.getCurrentHash()
+        (block['hash'], nonce) = self.getHash(block)
+        block['nonce'] = nonce
+        self.summitMinnedBlock(block)
 
 
 if __name__ == '__main__':
